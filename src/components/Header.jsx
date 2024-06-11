4af4f6b02df9797f675c2d6d798c8814fa131a01
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-11 16:28:24 +0530

    auth & react-redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+  const navigate = useNavigate();
+  const user = useSelector((store)=> store.user)
+
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate("/");
+    }).catch((error) => {
+      // An error happened.
+      navigate("/error")
+    });
+  }
+
   return (
-    <div className="absolute top-0 left-0 p-4 z-10 flex items-center w-full">
+    <div className="absolute top-0 left-0 px-4 py-2 z-10 flex items-center w-full bg-gradient-to-b from-black">
       <img
-        className="w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 mx-2 rounded-full"
+        className="w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 mx-2 my-1 rounded-full"
         src="https://media.istockphoto.com/id/1642381175/vector/cinema.jpg?s=612x612&w=0&k=20&c=owIct55daWlWRwPbTYLI9Y1IsrgYiqJcpvvgycvxBhE="
         alt="Logo"
       />
@@ -16,6 +35,14 @@ const Header = () => {
           Your Personalized Movie Guide
         </h3> */}
       </div>
+
+     {user && <div className='flex ml-auto'>
+        <img className='w-12 h-12 rounded-md' src={user?.photoURL} alt="userIcon" />
+        <button className=' h-9 px-1 rounded-md text-sm mt-1 text-white bg-red-700 ml-4 hover:bg-red-600 active:scale-95 focus:outline-none'
+        onClick={handleSignOut}>
+          Sign Out</button>
+      </div>
+      }   
     </div>
   );
 };
