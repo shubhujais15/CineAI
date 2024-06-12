405283ff0bf9a11b1cf4a058dcb981137d7ac3c4
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-12 22:32:18 +0530

    Unsubscribe from store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,45 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { useEffect } from 'react';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((store)=> store.user)
 
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
       // Sign-out successful.
-      navigate("/");
+      // navigate("/");
     }).catch((error) => {
       // An error happened.
       navigate("/error")
     });
   }
+
+  useEffect(()=>{
+   const unsubscribe =  onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          const {uid, email ,displayName, photoURL} = user;
+          dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}))
+          navigate("/browse")
+          // ...
+        } else {
+          // User is signed out
+          // ...
+          dispatch(removeUser());
+          navigate("/")
+        }
+      });
+      return() => unsubscribe();
+  },[]);
 
   return (
     <div className="absolute top-0 left-0 px-4 py-2 z-10 flex items-center w-full bg-gradient-to-b from-black">
