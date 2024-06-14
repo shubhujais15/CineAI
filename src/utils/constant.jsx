d1072685a26a1208b1c0e4297f374afa3c2bf85f
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-14 23:49:52 +0530

    constants

---
+++ src/utils/constant.jsx
@@ -0,0 +1,13 @@
+export const App_Logo = "https://media.istockphoto.com/id/1642381175/vector/cinema.jpg?s=612x612&w=0&k=20&c=owIct55daWlWRwPbTYLI9Y1IsrgYiqJcpvvgycvxBhE="
+
+export const USER_LOGO = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-dyrp6bw6adbulg5b.jpg"
+
+export const BACK_PIC = "https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjA4YzlmNmZjOTA5MTJmMDZkNzRjM2NhNzk5NTc4NSIsInN1YiI6IjY2NmIzMDc4Y2FmNjI4MWVlNDM3MDAwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cdZIwDIBDMfyHDuhgUAdSBEpuUyZUBvf6aa72GVfjvc'
+    }
+  };
