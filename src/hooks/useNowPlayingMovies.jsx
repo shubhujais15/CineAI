cc63f2567d887f70eae119ec6152de3b55d6bc84
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-15 23:59:38 +0530

    Movie Video api

---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+import { options } from "../utils/constant";
+
+const useNowPlayingMovies = () => {
+
+    const dispatch = useDispatch();
+  
+    const getNowPlayingMovie = async() => {
+      const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+      const json = await data.json();
+      // console.log(json.results)
+      dispatch(addNowPlayingMovies(json.results))
+    }
+  
+    useEffect(() => {
+      getNowPlayingMovie();
+    },[])
+}
+
+
+export default useNowPlayingMovies;
