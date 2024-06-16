efdbc14bfaade5f9c2b3bb6ca54720b0ced1ef93
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-16 23:18:13 +0530

    popular movie api

---
+++ src/hooks/usePopularMovies.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addPopularMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+import { options } from "../utils/constant";
+
+const usePopularMovies = () => {
+
+    const dispatch = useDispatch();
+  
+    const getPopularMovie = async() => {
+      const data = await fetch('https://api.themoviedb.org/3/movie/popular?&page=1', options)
+      const json = await data.json();
+      // console.log(json.results)
+      dispatch(addPopularMovies(json.results))
+    }
+  
+    useEffect(() => {
+      getPopularMovie();
+    },[])
+}
+
+
+export default usePopularMovies;
