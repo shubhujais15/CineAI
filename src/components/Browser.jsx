cfeca4b6a95580d7ac2227b4683ccbd745ca9b48
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-22 23:16:14 +0530

    Movie browse page

---
+++ src/components/Browser.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from "react-redux";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import GPTSearch from "./GPTSearch";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+
+const Browser = () => {
+
+  const getSearchView = useSelector((store)=> store.gpt.gptSearchView)
+
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
+
+  return (
+    <div>
+      <Header />
+      {
+        getSearchView ? <GPTSearch /> : <><MainContainer />
+      <SecondaryContainer /></>
+      }
+      
+      
+    </div>
+  )
+}
+
+export default Browser;
