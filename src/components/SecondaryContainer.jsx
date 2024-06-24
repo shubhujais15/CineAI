30410ce029e1afd2e0c44a8545627e2580d83116
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-24 23:29:03 +0530

    Retrive movie list

---
+++ src/components/SecondaryContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MovieList from './MovieList';
+import { useSelector } from 'react-redux';
+
+const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies)
+  return (
+    movies.nowPlayingMovies && (
+    <div className='bg-black'>
+      <div className='-mt-32 relative z-50'>
+      <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies?.popularMovies}/>
+      <MovieList title={"Top Rated"} movies={movies?.topRatedMovies}/>
+      <MovieList title={"Upcoming"} movies={movies?.upcomingMovies}/>
+      </div>
+    </div>
+    )
+  )
+}
+
+export default SecondaryContainer;
