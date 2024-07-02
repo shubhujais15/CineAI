f48f8b8710763ff601f542ff19c960670e464b67
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-02 23:58:47 +0530

    change css for responsive

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import { IMG_CDN_URL } from '../utils/constant';
 const MovieCard = ({posterPath}) => {
   if(!posterPath) return null;
   return (
-    <div className='w-48 pr-4'>
+    <div className='w-32 md:w-48 pr-4'>
         <img src={IMG_CDN_URL+ posterPath} alt="Movie Card" />
     </div>
   )
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -7,7 +7,7 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
     <div className='bg-black'>
-      <div className='-mt-32 relative z-50'>
+      <div className='mt-0 md:-mt-32 pl-1 md:pl-0 relative z-50'>
       <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies}/>
       <MovieList title={"Popular"} movies={movies?.popularMovies}/>
       <MovieList title={"Top Rated"} movies={movies?.topRatedMovies}/>
